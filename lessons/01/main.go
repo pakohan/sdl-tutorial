77a02dbbbab2ad5e5aebef9ae7ec661949e4e26b
Patrick Kohan <patrick.kohan@gmail.com>
2013-02-13 21:49:50 +0100

    ported hotfix manually to develop/feature branch

--- lessons/01/main.go
+++ lessons/01/main.go
@@ -4,16 +4,6 @@ import (
 	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
 )
 
-func loadImage(name string) *sdl.Surface {
-	image := sdl.Load(name)
-
-	if image == nil {
-		panic(sdl.GetError())
-	}
-
-	return image
-}
-
 func main() {
 	if sdl.Init(sdl.INIT_EVERYTHING) != 0 {
 		panic(sdl.GetError())
